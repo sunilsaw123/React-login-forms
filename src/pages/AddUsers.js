805e2194ce74d061bd88e8481a8cf570267960c6
sunil.saw <sunil.saw@softvision.com>
2018-09-20 16:43:51 +0530

    Router and loging form

---
+++ src/pages/AddUsers.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import SignUpForm from './SignUpForm.js';
+
+class AddUsers extends Component{
+    render(){
+        return(
+            <h1>Hi </h1>
+        );
+    }
+}
+export default AddUsers;
